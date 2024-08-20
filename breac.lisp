6fb17dc1c7d4669d7bd6faf0675771efd44fb33d
Eugene Zaikonnikov <eugene@funcall.org>
2024-08-20 22:29:28 +0200

    implement basic wrapping break

--- breac.lisp
+++ breac.lisp
@@ -3,7 +3,12 @@
 (defparameter *breac-points* '())
 
 (defmacro breac (&optional tag step &body body)
-  )
+  (let ((result (gensym)))
+    `(let ((,result ,body))
+       ,(if tag
+	    (error "Not implemented")
+	    '(break))
+       ,result)))
 
 (defmacro breac-if (test &optional tag step &body body)
   )
