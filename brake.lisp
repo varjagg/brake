5981c2fefa192218fb0c6397838ae9e26c9358e7
Eugene Zaikonnikov <eugene@funcall.org>
2024-08-23 00:30:43 +0200

    implement most of brake functionality

--- brake.lisp
+++ brake.lisp
@@ -14,21 +14,41 @@
   (let ((record (or (gethash tag *brake-records*)
 		    (setf (gethash tag *brake-records*)
 			  (make-instance 'brake-record)))))
-    (setf (brake-points record) (merge 'list (list step)
-				       (brake-points record)
-				       #'<))))
+    (unless (find step (brake-points record))
+      (setf (brake-points record)
+	    (merge 'list (list step)
+		   (brake-points record)
+		   #'<)))
+    record))
 
-(defmacro brake (&optional tag-or-body step &body body)
-  (let ((result (gensym "BRK")))
-    (when (and tag-or-body (listp tag-or-body))
-      (setf body tag-or-body))
-    `(let ((,result (progn ,@body)))
-       ,(if tag-or-body
-	    (error "Not implemented")
+(defmacro brake (&optional tag-or-sexp step sexp)
+  (let ((result (gensym "BRK-RES"))
+	(record (gensym "BRK"))
+	(tail (gensym "BRK")))
+    (when (and tag-or-sexp (listp tag-or-sexp))
+      (setf sexp tag-or-sexp))
+    `(let ((,result (progn ,sexp)))
+       ,(if tag-or-sexp
+	    (if (keywordp tag-or-sexp)
+		(progn
+		  (add-brake-record tag-or-sexp step)
+		  `(let ((,record (gethash ,tag-or-sexp ,*brake-records*)))
+		     (unless record
+		       (error "No record found for breakpoing with tag ~a" tag-or-sexp))
+		     (let ((,tail (member (state ,record) (brake-points ,record))))
+		       ;; right after current
+		       (unwind-protect
+			    (when (eql (cdr ,tail) (member ,step ,tail))
+			      (break)
+			      (setf (state ,record) ,step))
+			 ;; reset state if user aborts from BREAK or after last break
+			 (unless (eql (state ,record) ,step)
+			   (setf (state ,record) 0))))))
+		'(break))
 	    '(break))
        ,result)))
 
-(defmacro brake-when (test &optional tag step &body body)
+(defmacro brake-when (predicate &optional tag step sexp)
   )
 
 (defmacro break-when (&rest args)
