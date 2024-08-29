9513f27d7c342a363c47d615385f93607d52176e
Eugene Zaikonnikov <eugene@funcall.org>
2024-08-29 23:36:37 +0200

    export

--- package.lisp
+++ package.lisp
@@ -1,4 +1,4 @@
 (defpackage #:brake
   (:use #:common-lisp)
   (:export #:brake #:brake-when #:break-when #:brake-disable #:brake-enable
-	   #:reset-brake-points #:reset-brake-tag #:mark))
+	   #:reset-brake-points #:reset-brake-tag #:mark #:brake-reset))
